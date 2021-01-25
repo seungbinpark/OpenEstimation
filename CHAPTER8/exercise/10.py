a5d9b178e40c785d8ffd4d347922a5e82dda79f1
seungbinpark <70265096+seungbinpark@users.noreply.github.com>
2021-01-25 18:35:45 +0900

    Update 10.py

--- CHAPTER8/exercise/10.py
+++ CHAPTER8/exercise/10.py
@@ -15,10 +15,14 @@ def translate(img, pt):
 image = cv2.imread("images/translate.jpg", cv2.IMREAD_GRAYSCALE)
 if image is None: raise Exception("영상파일 읽기 에러")
 
-dst1 = translate(image, (30, 80))
-dst2 = translate(image, (-70, -50))
+dst1 = translate(image, (50, 60))
+
+pt1 = np.array([(0, 0), (0, image.shape[1]), (image.shape[0], 0)], np.float32)
+pt2 = np.array([(50, 60), (50, 60 + image.shape[1]), (50 + image.shape[0], 60)], np.float32)
+aff_mat = cv2.getAffineTransform(pt1, pt2)
+dst2 = cv2.warpAffine(image, aff_mat, image.shape[:2])
 
 cv2.imshow("image", image)
-cv2.imshow("dst1: trans to (30, 80)", dst1)
-cv2.imshow("dst2: trans to (-70, -50)", dst2)
+cv2.imshow("dst1", dst1)
+cv2.imshow("dst2", dst2)
 cv2.waitKey(0)
